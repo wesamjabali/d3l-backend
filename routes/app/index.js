386ff70431beb6563636776a4b63f1faa1835d3f
Wesam Jabali <wesamjabali@icloud.com>
2021-02-18 17:47:38 -0600

    added more seeds

--- routes/app/index.js
+++ routes/app/index.js
@@ -3,17 +3,21 @@ const express = require("express");
 const router = express.Router();
 
 // controller files imports
-const authRoutes = require("./auth");
-const accountRoutes = require("./account");
+const non_auth_routes = require("./non_auth");
+const admin_routes = require("./admin");
+const faculty_routes = require("./faculty");
+const auth_routes = require("./auth");
 
 // auth middleware
-const authMiddleware = require("../../middleware/authMiddleware")
+const auth_middleware = require("../../middleware/authMiddleware");
+const roles_middleware = require("../../middleware/rolesMiddleware");
 
 // routers attached
-router.use("/auth", authRoutes);
+router.use("/non_auth", non_auth_routes);
 // protected subrouter
-router.use("/account", authMiddleware, accountRoutes)
-
+router.use("/admin", roles_middleware(['admin']), admin_routes);
+router.use("/faculty", roles_middleware(['faculty']), faculty_routes);
+router.use("/auth", auth_middleware, auth_routes);
 
 // export router
 module.exports = router;
