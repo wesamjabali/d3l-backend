2a8213288c80476699f4ec3eff296db8dc257065
Wesam Jabali <wesam@mini.local>
2021-02-19 04:26:39 -0600

    Use auth middleware everywhere

--- middleware/rolesMiddleware.js
+++ middleware/rolesMiddleware.js
@@ -1,36 +1,31 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (roles = []) => {
-  
-    return (req, res, next) => {
-      // grab admin roles
-      if(!req.headers.authorization) {
-        res.status(401).json({"401": "User not logged in."});
-        throw new Error("User not logged in.");
+  return (req, res, next) => {
+    // grab user roles
+    const token = req.headers.authorization.split(" ")[1];
+    const user = jwt.verify(token, process.env.AUTH_CLIENT_SECRET);
+
+    const userRoles = user.roles;
+    // if includes admin, autopass
+    if (userRoles.includes("admin")) {
+      return next();
+    } else {
+      // check if user has any of the roles assigned
+      let hasRole = false;
+      for (let role of roles) {
+        if (userRoles.includes(role)) {
+          hasRole = true;
+        }
       }
-      const token = req.headers.authorization.split(' ')[1];
-      const user = jwt.verify(token, process.env.AUTH_CLIENT_SECRET);
-      
-      const userRoles = user.roles;
-      // if includes admin, autopass
-      if (userRoles.includes("admin")) {
-        return next();
+      // if true proceed, otherwise reject
+      console.info("TCL: hasRole", hasRole);
+      if (hasRole) {
+        next();
       } else {
-        // check if user has any of the roles assigned
-        let hasRole = false;
-        for (let role of roles) {
-          if (userRoles.includes(role)) {
-            hasRole = true;
-          }
-        }
-        // if true proceed, otherwise reject
-        console.info("TCL: hasRole", hasRole);
-        if (hasRole) {
-          next();
-        } else {
-          // reject otherwise
-          res.status(403).json({});
-        }
+        // reject otherwise
+        res.status(403).json({});
       }
-    };
+    }
   };
+};
--- routes/app/index.js
+++ routes/app/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const non_auth_routes = require("./non_auth");
 const admin_routes = require("./admin");
 const faculty_routes = require("./faculty");
-const auth_routes = require("./auth");
+const user_routes = require("./user");
 
 // auth middleware
 const auth_middleware = require("../../middleware/authMiddleware");
@@ -15,9 +15,19 @@ const roles_middleware = require("../../middleware/rolesMiddleware");
 // routers attached
 router.use("/non_auth", non_auth_routes);
 // protected subrouter
-router.use("/admin", roles_middleware(['admin']), admin_routes);
-router.use("/faculty", roles_middleware(['faculty']), faculty_routes);
-router.use("/auth", auth_middleware, auth_routes);
+router.use(
+  "/admin",
+  auth_middleware,
+  roles_middleware(["admin"]),
+  admin_routes
+);
+router.use(
+  "/faculty",
+  auth_middleware,
+  roles_middleware(["faculty"]),
+  faculty_routes
+);
+router.use("/user", auth_middleware, user_routes);
 
 // export router
 module.exports = router;
