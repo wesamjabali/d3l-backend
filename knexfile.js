26367840f3765ffd2b39368d36f09e13198f1860
Wesam Jabali <wesamjabali@icloud.com>
2021-02-19 07:20:28 -0600

    ps

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + "?sslmode=require",
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + "/database/migrations",
     },
