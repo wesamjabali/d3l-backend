fd5a4913f86fe38e2bf7a2cf3978a738cbb5a639
Wesam Jabali <wesamjabali@icloud.com>
2021-02-22 20:38:22 -0600

    nothing.

--- routes/app/faculty/course.js
+++ routes/app/faculty/course.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../../../database/knex");
 
+// Get all courses
+router.get("/getAllCourses", async (req, res, next) => {
+    try {
+      const courses = await knex
+        .select(["id", "title"])
+        .where({'course_id': course_id})
+        .from("d3l_course");
+      res.status(200).json({ courses });
+    } catch (err) {
+      next(err);
+    }
+  });
 
+  
 
 module.exports = router;
