e52d54db1efbcd7524e57803bf1ca7a7ee7fa857
Joseph "Joey" Navarro <jnavar43@depaul.edu>
2021-02-23 18:34:45 -0600

    Added more team functionality to faculty

--- routes/app/faculty/team.js
+++ routes/app/faculty/team.js
@@ -3,38 +3,134 @@ const router = express.Router();
 const knex = require("../../../database/knex");
 const jwt = require("jsonwebtoken");
 
-// New Team:
-// get title from frontend
-// get course_id from frontend
-// check if that team already exists for that course
-// if not, create that team
-// if so, return 409 error
+// Create a new team
 router.post("/new", async (req, res, next) => {
   try {
     const { team_name, course_id } = req.body;
 
-    let existing_course = await knex("d3l_team")
+    // Ensure target team does not exist yet
+    let existing_team = await knex("d3l_team")
       .where({
         team_name: team_name,
         course_id: course_id,
       })
       .select("id");
 
-    if (existing_course.length > 0) {
+    if (existing_team.length > 0) {
       res.status(409).json({});
-      throw new Error("Teams already exists.");
+      throw new Error("Team already exists.");
     }
 
+    // Update table
     await knex("d3l_team").insert({
         team_name: team_name,
         course_id: course_id,
     });
+    res.status(201).json({});
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Delete existing team
+router.post("/delete", async (req, res, next) => {
+  try {
+    const { team_name, course_id } = req.body;
+
+    /*
+    // Ensure target team exists
+    let existing_team = await knex("d3l_team")
+      .where({
+        team_name: team_name,
+        course_id: course_id,
+      })
+      .select("id");
+
+    if (existing_team.length == 0) {
+      res.status(409).json({});
+      throw new Error("Specified team does not exist.");
+    }
+    */
+
+    // Update table
+    await knex("d3l_team")
+      .where({
+        team_name: team_name,
+        course_id: course_id,
+      })
+      .del();
+
     res.status(201).json({});
   } catch (err) {
     next(err);
   }
 });
 
-// query courses for associated users include my user id
-// if null, return auth error
+// Add user to a team
+router.post("/addUser", async (req, res, next) => {
+  try {
+    const { user_id, team_id } = req.body;
+
+    // Can't add user if they're already part of target team
+    let existing_result = await knex("d3l_user_team")
+      .where({
+        user_id: user_id,
+        team_id: team_id,
+      })
+      .select();
+
+    if (existing_result.length > 0) {
+      res.status(409).json({});
+      return;
+    }
+
+    // Update team table
+    await knex("d3l_user_team").insert({
+      user_id: user_id,
+      team_id: team_id,
+    });
+    
+    res.status(201).json({});
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Remove user from team
+router.post("/deleteUser", async (req, res, next) => {
+  try {
+    const { user_id, team_id } = req.body;
+
+    /*
+    // Can't delete user if they're not part of target team
+    let existing_result = await knex("d3l_user_team")
+      .where({
+        user_id: user_id,
+        team_id: team_id,
+      })
+      .select();
+
+    if (existing_result.length == 0) {
+      res.status(409).json({});
+      return;
+    }
+    */
+
+    // Update team table
+    await knex("d3l_user_team")
+      .where({
+        user_id: user_id,
+        team_id: team_id,
+      })
+      .del();
+    
+    res.status(201).json({});
+    
+  } catch (err) {
+    next(err);
+  }
+});
+
+
 module.exports = router;
