7a3fb1e1d4ad73afa0aab9a7aafc1f5adb959f88
Wesam Jabali <wesamjabali@icloud.com>
2021-02-23 18:35:50 -0600

    Update routes for user

--- routes/app/user/index.js
+++ routes/app/user/index.js
@@ -3,9 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const course_routes = require("./course.js");
+const content_routes = require("./content.js");
+const team_routes = require("./team.js");
 
 router.use("/course", course_routes);
-
+router.use("/content", content_routes);
+router.use("/team", team_routes);
 
 // export router
 module.exports = router;
