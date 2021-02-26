607b2478b053447d67a8e11f90f4a439d34d1831
Wesam Jabali <wesamjabali@icloud.com>
2021-02-25 20:15:09 -0600

    Add delete own user functionality to user accounts

--- routes/app/user/index.js
+++ routes/app/user/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const course_routes = require("./course.js");
 const content_routes = require("./content.js");
 const team_routes = require("./team.js");
+const user_routes = require("./user.js");
 
 router.use("/course", course_routes);
 router.use("/content", content_routes);
 router.use("/team", team_routes);
+router.user("/user", user_routes);
 
 // export router
 module.exports = router;
