ab0f96f778231ec7ccf35186745490cd897d969d
Wesam Jabali <wesamjabali@icloud.com>
2021-02-25 20:56:33 -0600

    Add discussion post edit functionality to user accounts

--- routes/app/user/discussion.js
+++ routes/app/user/discussion.js
@@ -121,10 +121,30 @@ router.post("/delete", async(req, res, next) => {
       .del();
 
     res.status(200).json({});
-    
+
   } catch (err) {
       next(err);
   }
+});
+
+// Edit a discussion post
+router.post("/edit", async(req, res, next) => {
+  try {
+    const { post_id, title, body } = req.body;
+
+    await knex("d3l_discussion_post")
+      .where({ id: post_id })
+      .update({
+          title: title,
+          body: body
+      });
+    
+      res.status(201).json({});
+
+  } catch (err) {
+    next(err);
+  }
+
 });
 
 module.exports = router;
