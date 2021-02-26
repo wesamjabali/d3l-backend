1318aac661d6379c5b18b0824bb189e8bf9af96a
Joseph "Joey" Navarro <jnavar43@depaul.edu>
2021-02-25 21:12:42 -0600

    Split discussion deletion/editing based on role privileges

--- routes/app/faculty/index.js
+++ routes/app/faculty/index.js
@@ -6,11 +6,13 @@ const user_routes = require("./user.js");
 const course_routes = require("./course.js");
 const team_routes = require("./team.js");
 const content_routes = require("./content.js");
+const discussion_routes = require("./discussion.js");
 
 router.use("/user", user_routes);
 router.use("/course", course_routes);
 router.use("/team", team_routes);
 router.use("/content", content_routes);
+router.user("/discussion", discussion_routes);
 
 // export router
 module.exports = router;
