b2888f6334352a0e3c579278bf683b8cfe425b3f
Wesam Jabali <wesamjabali@icloud.com>
2021-02-25 22:58:08 -0600

    add course belongs route

--- routes/app/user/course.js
+++ routes/app/user/course.js
@@ -31,11 +31,39 @@ router.get("/getAllCourses", async (req, res, next) => {
   const user = jwt.verify(token, process.env.AUTH_CLIENT_SECRET);
   try {
     const courses = await knex
-      .select("d3l_course.id", "title")
+      .select(
+        "d3l_course.id",
+        "title",
+        "course_number",
+        "course_prefix",
+        "section_number"
+      )
       .from("d3l_course")
       .join("d3l_user_course", "d3l_user_course.course_id", "d3l_course.id")
       .where({ "d3l_user_course.user_id": user.id });
     res.status(200).json({ courses });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Check if a user belongs to a course.
+router.get("/belongs", async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const user = jwt.verify(token, process.env.AUTH_CLIENT_SECRET);
+  const { course_id } = req.query;
+
+  try {
+    const courses = await knex
+      .select("id")
+      .from("d3l_user_course")
+      .where({ user_id: user.id, course_id: course_id });
+
+    if (courses.length == 0) {
+      return res.status(200).json({ value: false });
+    } else {
+      return res.status(200).json({ value: true });
+    }
   } catch (err) {
     next(err);
   }
