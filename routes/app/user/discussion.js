7b788500df51308fe48988dbfc29a1e92a72c35e
Joseph "Joey" Navarro <jnavar43@depaul.edu>
2021-02-25 20:28:26 -0600

    Add discussion post functionality to user accounts

---
+++ routes/app/user/discussion.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../../../database/knex");
+const jwt = require("jsonwebtoken");
+
+// Get all the relevant fields of a given discussion post (post ID)
+router.get("/getDiscussionPost", async (req, res, next) => {
+  const { post_id } = req.query;
+
+  try {
+    const results = await knex
+      .select(["user_id", "course_id", "parent_id", "content_id", "title", "body"])
+      .from("d3l_discussion_post")
+      .where({ id: post_id });
+
+      res.status(200).json({ results });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get all discussion posts (post IDs) within a given course
+router.get("/getAllByCourse", async (req, res, next) => {
+  const { course_id } = req.query;
+
+  try {
+      const posts = await knex
+        .select("id")
+        .from("d3l_discussion_post")
+        .where({ course_id: course_id });
+
+      res.status(200).json({ posts });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get all discussion posts (post IDs) under a given content
+router.get("/getAllByContent", async (req, res, next) => {
+  const { content_id } = req.query;
+  
+  try {
+      const posts = await knex
+        .select("id")
+        .from("d3l_discussion_post")
+        .where({ content_id: content_id });
+
+      res.status(200).json({ posts });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get all discussion posts (post IDs) under a given parent post
+router.get("/getAllByParent", async (req, res, next) => {
+  const { parent_id } = req.query;
+
+  try {
+      const posts = await knex
+        .select("id")
+        .from("d3l_discussion_post")
+        .where({ parent_id: parent_id });
+
+      res.status(200).json({ posts });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get all discussion posts (post IDs) associated with a given user
+router.get("/getAllByUser", async (req, res, next) => {
+  const { user_id } = req.query;
+
+  try {
+    const posts = await knex
+      .select("id")
+      .from("d3l_discussion_post")
+      .where({ user_id: user_id});
+    
+    res.status(200).json({ posts });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- routes/app/user/index.js
+++ routes/app/user/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const course_routes = require("./course.js");
 const content_routes = require("./content.js");
 const team_routes = require("./team.js");
+const discussion_routes = require('./discussion.js');
 
 router.use("/course", course_routes);
 router.use("/content", content_routes);
 router.use("/team", team_routes);
+router.use("./discussion", discussion_routes);
 
 // export router
 module.exports = router;
